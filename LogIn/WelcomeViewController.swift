a68167af61d1b045970a98903eef67d5119050b6
Mikhail <jegnum@gmail.com>
2022-02-19 17:05:03 +0300

    Create ViewController

---
+++ LogIn/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  LogIn
+//
+//  Created by Михаил Светлов on 19.02.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
