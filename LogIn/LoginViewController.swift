c732f6485ff9870a635ccc53ac70c475125d0dff
Mikhail <jegnum@gmail.com>
2022-02-20 18:52:39 +0300

    IBAction prepare

--- LogIn/LoginViewController.swift
+++ LogIn/LoginViewController.swift
@@ -10,26 +10,42 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var userNameTitle: UITextField!
     
-   
-let test = 1
+    @IBOutlet weak var PasswordTitle: UIStackView!
+    var userName: String!
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     
     }
 
     @IBAction func loginAction(_ sender: Any) {
-        if test != 1 {
+        let test = userNameTitle.text
+        if test != "1" {
             showAlert(with: "bla", and: "blabla")
+            
         }
     }
+    
+    @IBAction func forgotPassword() {
+    }
+    
+    @IBAction func forgotName() {
+    }
+    
+    
     
 }
 extension ViewController {
     private func showAlert(with title: String, and massage: String) {
         let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
         present(alert, animated: true)
     }
 }
