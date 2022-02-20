f569f8f3c8e36d940e92cba970aae601d93a4d72
Mikhail <jegnum@gmail.com>
2022-02-20 18:16:32 +0300

    Navigation controller Done

--- LogIn/LoginViewController.swift
+++ LogIn/LoginViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+   
+let test = 1
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
 
-
+    @IBAction func loginAction(_ sender: Any) {
+        if test != 1 {
+            showAlert(with: "bla", and: "blabla")
+        }
+    }
+    
+}
+extension ViewController {
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        present(alert, animated: true)
+    }
 }
 
