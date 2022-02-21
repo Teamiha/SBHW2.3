df620b3aabd1c7fc17ca5f5af3bad01ca1256b62
Mikhail <jegnum@gmail.com>
2022-02-21 18:39:03 +0300

    gradient Done

--- LogIn/WelcomeViewController.swift
+++ LogIn/WelcomeViewController.swift
@@ -11,17 +11,12 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Create a gradient layer.
         let gradientLayer = CAGradientLayer()
-        // Set the size of the layer to be equal to size of the display.
         gradientLayer.frame = view.bounds
-        // Set an array of Core Graphics colors (.cgColor) to create the gradient.
-        // This example uses a Color Literal and a UIColor from RGB values.
-        gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, UIColor(red: 252/255, green: 238/255, blue: 33/255, alpha: 1).cgColor]
-        // Rasterize this static layer to improve app performance.
+        gradientLayer.colors = [#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1).cgColor, #colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor]
         gradientLayer.shouldRasterize = true
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
-
+   
 }
